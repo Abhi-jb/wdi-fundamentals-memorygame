e1b360a7f9ceb800548f8ecc2d09349a50c5c312
Abhi-jb <janthulurabhilash@gmail.com>
2019-09-26 13:43:20 +1000

    created game logic-js added

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,13 @@
-var cardOne = "King";
-var cardTwo = "King";
-var cardThree = "Queen";
-var cardFour = "Queen";
-console.log("user Flipped" + cardOne);
-console.log("user Flipped" + cardFour) ;
+var cards=["Queen","Queen", "King", "King"];
+var cardsInPlay=[];
+var cardOne=cards[0];
+cardsInPlay.push(cardOne);
+console.log('user flipped' +" " +cardsInPlay);
+var cardTwo=cards[1];
+cardsInPlay.push(cardTwo);
+console.log('user flipped' + " "+cardsInPlay[1]);
+if(cardsInPlay.length === 2 && cardsInPlay[0]===cardsInPlay[1] ){
+  alert("You Found A Match");
+}else{
+  alert("Sorry Try Again");
+}
