e4335fb404392bc3413f7e7534edf17d7bb71536
Abhi-jb <janthulurabhilash@gmail.com>
2019-09-28 20:16:56 +1000

    created game logic(Objects added)

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,22 @@
-var cards=["Queen","Queen", "King", "King"];
+var cards=[
+  {
+    name:"queen",
+    suit:"love",
+    path:"images/queen-of-hearts.png"
+  }, {
+  name:"queen",
+  suit:"diamond",
+  path:"images/queen-of-diamonds.png"
+}, {
+  name:"king",
+  suit:"heart",
+  path:"images/king-of-hearts.png"
+}, {
+  name:"king",
+  suit:"diamond",
+  path:"images/king-of-diamonds.png"
+}
+];
 var cardsInPlay=[];
 function checkForMatch(){
   if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -10,11 +28,13 @@ function checkForMatch(){
 
 function flipCard(cardId){
 
-  var cardToBePushed =  cards[cardId];
+  var cardToBePushed =  cards[cardId].name;
   cardsInPlay.push(cardToBePushed);
   console.log("user flipped"+" "+cardToBePushed);
-  }
+  console.log(cards[cardId].suit);
+  console.log(cards[cardId].path);
+}
 
 flipCard(0);// cardId = 0
-flipCard(1);// cardId = 1
+flipCard(1);
 checkForMatch();
